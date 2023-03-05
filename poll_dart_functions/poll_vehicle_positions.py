4b6c4c013f6efe27b207b212768c4453fd07d959
Paul Rowe <prowe@sourceallies.com>
2023-03-05 17:19:36 -0600

    swiching to aurora

--- poll_dart_functions/poll_vehicle_positions.py
+++ poll_dart_functions/poll_vehicle_positions.py
@@ -1,10 +1,12 @@
 import requests
 import gtfs_realtime_pb2
-import boto3
 import os
-from datetime import datetime, tzinfo
+import json
+import csv
+import pg8000.native
+from datetime import datetime
+from io import StringIO
 
-redshift_client = boto3.client('redshift-data')
 
 create_landing_table_sql = """
     CREATE TABLE IF NOT EXISTS vehicle_position_events (
@@ -22,12 +24,20 @@ create_landing_table_sql = """
     );
 """
 
-def __create_table_if_needed():
-    redshift_client.execute_statement(
-        Database=os.environ.get('DB_NAME'),
-        WorkgroupName=os.environ.get('WORKGROUP_NAME'),
-        Sql=create_landing_table_sql
-    )
+
+copy_in_statement = """
+    COPY vehicle_position_events 
+        (timestamp, vehicle_id, vehicle_label, trip_trip_id, trip_start_date, trip_route_id, trip_direction_id, position_latitude, position_longitude, position_bearing, occupancy_status) 
+    FROM STDIN WITH (FORMAT CSV)
+"""
+
+
+def __get_database_secret():
+    secret_arn = os.environ.get('DATABASE_SECRET_ARN')
+    secret_url = f"http://localhost:2773/secretsmanager/get?secretId={secret_arn}"
+    r = requests.get(secret_url, headers={"X-Aws-Parameters-Secrets-Token": os.environ.get('AWS_SESSION_TOKEN')})
+    secret = json.loads(r.text)["SecretString"]
+    return json.loads(secret)
 
 
 def __get_vehicle_positions():
@@ -37,35 +47,42 @@ def __get_vehicle_positions():
     feed_message.ParseFromString(response.content)
     return feed_message
 
-insert_statement = """
-    INSERT INTO vehicle_position_events (timestamp, vehicle_id, vehicle_label, trip_trip_id, trip_start_date, trip_route_id, trip_direction_id, position_latitude, position_longitude, position_bearing, occupancy_status)
-    VALUES (:timestamp, :vehicle_id, :vehicle_label, :trip_trip_id, :trip_start_date, :trip_route_id, :trip_direction_id, :position_latitude, :position_longitude, :position_bearing, :occupancy_status)
-"""
+
+def __iterate_vehicle_positions():
+    feed_message = __get_vehicle_positions()
+    print(feed_message)
+    txt_stream = StringIO()
+    csv_writer = csv.writer(txt_stream)
+    for entity in feed_message.entity:
+        csv_writer.writerow([
+            datetime.fromtimestamp(entity.vehicle.timestamp).isoformat(),
+            entity.vehicle.vehicle.id,
+            entity.vehicle.vehicle.label,
+            entity.vehicle.trip.trip_id,
+            entity.vehicle.trip.start_date,
+            entity.vehicle.trip.route_id,
+            str(entity.vehicle.trip.direction_id),
+            str(entity.vehicle.position.latitude),
+            str(entity.vehicle.position.longitude),
+            str(entity.vehicle.position.bearing),
+            str(entity.vehicle.occupancy_status)
+        ])
+    txt_stream.seek(0)
+    return txt_stream
 
 
 def handler(event, context):
     print('handling event', event)
-    feed_message = __get_vehicle_positions()
-    print(feed_message)
-    __create_table_if_needed()
-    for entity in feed_message.entity:
-        execute_response = redshift_client.execute_statement(
-            Database=os.environ.get('DB_NAME'),
-            WorkgroupName=os.environ.get('WORKGROUP_NAME'),
-            Sql=insert_statement,
-            Parameters=[
-                {"name": "timestamp", "value": datetime.fromtimestamp(entity.vehicle.timestamp).isoformat()},
-                {"name": "vehicle_id", "value": entity.vehicle.vehicle.id},
-                {"name": "vehicle_label", "value": entity.vehicle.vehicle.label},
-                {"name": "trip_trip_id", "value": entity.vehicle.trip.trip_id},
-                {"name": "trip_start_date", "value": entity.vehicle.trip.start_date},
-                {"name": "trip_route_id", "value": entity.vehicle.trip.route_id},
-                {"name": "trip_direction_id", "value": str(entity.vehicle.trip.direction_id)},
-                {"name": "position_latitude", "value": str(entity.vehicle.position.latitude)},
-                {"name": "position_longitude", "value": str(entity.vehicle.position.longitude)},
-                {"name": "position_bearing", "value": str(entity.vehicle.position.bearing)},
-                {"name": "occupancy_status", "value": str(entity.vehicle.occupancy_status)},
-            ]
-        )
-        print('Execute reponse ', execute_response)
 
+    secret = __get_database_secret()
+    con = pg8000.native.Connection(
+        host=os.environ.get('DATABASE_HOST'),
+        port=5432,
+        database='wh',
+        user=secret['username'],
+        password=secret['password'],
+        timeout=10,
+        ssl_context=True)
+    con.run(create_landing_table_sql)
+    con.run(copy_in_statement, stream=__iterate_vehicle_positions())
+    con.close()
--- run-dbt.sh
+++ run-dbt.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-export creds=$(aws redshift-serverless get-credentials --workgroup-name=dart-warehouse-wg)
-export DBT_PASSWORD=$(echo $creds | jq .dbPassword --raw-output)
-export DBT_USER=$(echo $creds | jq .dbUser --raw-output)
+export creds=$(aws secretsmanager get-secret-value \
+    --secret-id 'arn:aws:secretsmanager:us-east-1:144406111952:secret:rds!cluster-4f192915-bea1-421a-b447-70c70710b59b-6Zqh1y' \
+    --output=text --query='SecretString')
+export DBT_HOST=''
+export DBT_USER=$(echo $creds | jq .username --raw-output)
+export DBT_PASSWORD=$(echo $creds | jq .password --raw-output) 
 
 docker run -it \
     -v "$(pwd):/usr/app/dbt" \
@@ -15,4 +18,4 @@ docker run -it \
     -e DBT_USER \
     -e DBT_PASSWORD \
     -e DBT_PROFILES_DIR=/usr/app/dbt \
-    ghcr.io/dbt-labs/dbt-redshift "$@"
+    ghcr.io/dbt-labs/dbt-postgres "$@"
