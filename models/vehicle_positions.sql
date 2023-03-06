b033ab451f90e1fc528c4a2772f4febe5e974602
Paul Rowe <prowe@sourceallies.com>
2023-03-05 20:41:48 -0600

    adding a point type

--- models/vehicle_positions.sql
+++ models/vehicle_positions.sql
@@ -6,5 +6,7 @@
 
 select 
     "timestamp" as at_ts,
-    vehicle_id
+    vehicle_id::varchar(250) as vehicle_id,
+    ST_Point(position_longitude, position_latitude, 4326)::geography as position,
+    position_bearing
 from vehicle_position_events
