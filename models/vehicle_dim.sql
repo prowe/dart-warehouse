c1c9d4594626e45c8bb2928564d70089a9bc3bb1
Paul Rowe <prowe@sourceallies.com>
2023-03-10 19:26:20 -0600

    making incremental

--- models/vehicle_dim.sql
+++ models/vehicle_dim.sql
@@ -1,4 +1,8 @@
 SELECT DISTINCT
     vehicle_id as id,
-    last_value(vehicle_label) over (partition by vehicle_id order by timestamp) as label
+    last_value(vehicle_label) over (partition by vehicle_id order by timestamp) as label,
+    max(timestamp) over (partition by vehicle_id) as last_ts
 FROM {{ source('poll_dart', 'vehicle_position_events') }}
+{% if is_incremental() %}
+  where timestamp >= (select max(last_ts) from {{ this }})
+{% endif %}
--- models/vehicle_position_fact.sql
+++ models/vehicle_position_fact.sql
@@ -5,3 +5,6 @@ SELECT
     ST_Point(position_longitude, position_latitude, 4326)::geography as position,
     position_bearing
 FROM {{ source('poll_dart', 'vehicle_position_events') }}
+{% if is_incremental() %}
+  where timestamp >= (select max(at_ts) from {{ this }})
+{% endif %}
