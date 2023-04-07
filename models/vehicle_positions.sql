b01199af26404b07a2105f85a386961cfb3f97e1
Paul Rowe <prowe@sourceallies.com>
2023-04-06 20:47:50 -0500

    making progress

--- models/vehicle_positions.sql
+++ models/vehicle_positions.sql
@@ -1,19 +1,24 @@
 SELECT
-    to_date(ev.injest_date, 'yyyy/mm/dd') as injest_date,
-    cast(ev.injest_hour as INT) as injest_hour,
-    timestamp_seconds(ev.timestamp) as at_ts,
+    ev.injest_year_month as injest_year_month,
+    timestamp_seconds(cast(ev.timestamp as FLOAT)) as at_ts,
     
-    ev.trip.tripId as trip_id
+    ev.trip.tripId as trip_id,
+    to_date(ev.trip.startDate, 'yyyyMMdd') as trip_start_date,
+    ev.trip.routeId as trip_route_id,
+    ev.position.latitude as latitude,
+    ev.position.longitude as longitude,
+    ev.position.bearing as bearing,
+    ev.position.speed as speed,
 
-    -- start_date
-    -- route_id
-    -- direction_id
-    -- schedule_relationship
-    -- latitude
-    -- longitude
-    -- bearing
-    -- speed
-    -- vehicle_id
-    -- vehicle_label
-    -- occupancy_status
+    ev.vehicle.id as vehicle_id,
+    ev.vehicle.label as vehicle_label,
+
+    occupancyStatus as occupancy_status,
+
+    r.route_long_name,
+    r.route_type,
+    r.route_text_color,
+    r.route_color,
+    r.route_short_name
 FROM {{ source('poll_dart', 'vehicle_position_events') }} as ev
+LEFT JOIN {{ ref('routes') }} as r on ev.trip.routeId = r.route_id
